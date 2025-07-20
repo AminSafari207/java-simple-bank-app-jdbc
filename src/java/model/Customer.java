42b237479218ac519629672a778b88baa2410193
AminSafari207 <safarin207368@gmail.com>
2025-07-21 00:34:28 +0330

    Add id field to Customer model class + related getter

--- src/java/model/Customer.java
+++ src/java/model/Customer.java
@@ -12,6 +12,13 @@ public class Customer {
         this.phoneNumber = phoneNumber;
     }
 
+    public Customer(int id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
     public int getId() {
         return id;
     }
