173e627c83116398930191f18eb065ca4460ab52
AminSafari207 <safarin207368@gmail.com>
2025-07-21 00:36:05 +0330

    Add setter for name, email and phone number in Customer model class

--- src/java/model/Customer.java
+++ src/java/model/Customer.java
@@ -35,7 +35,15 @@ public class Customer {
         return phoneNumber;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
